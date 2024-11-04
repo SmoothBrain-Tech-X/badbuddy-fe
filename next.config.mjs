0d9fac6114f3afe21279387b9cf3852dbd6c33d0
Teerut26 <teerutsr@gmail.com>
2024-11-05 04:43:25 +0700

    refactor: remove eslint and experimental settings from Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,6 @@ const withBundleAnalyzer = bundleAnalyzer({
 export default withBundleAnalyzer({
   reactStrictMode: false,
   output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  },
   env: {
     NEXT_PUBLIC_BASE_API: 'https://general-badbuddy-be.tu4rl4.easypanel.host',
   },
